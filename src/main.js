5c0e6109d750fb5f490d6b78c2682ffdf478332a
Bruno Behnken <brunobehnken@gmail.com>
2023-05-01 00:06:24 -0300

    Adding main.js with Express logic

---
+++ src/main.js
@@ -0,0 +1,19 @@
+import express from "express";
+import validateCpf from "./validateCpf.js";
+import Order from "./order.js";
+
+const app = express();
+app.listen(3000);
+app.use(express.json());
+
+app.post('/checkout', async (req, res) => {
+    if (!validateCpf(req.body.cpf)) {
+        return res.json({ message: "Invalid CPF" });
+    }
+    const order = new Order();
+    const productsToAdd = req.body.products;
+    for (const product of productsToAdd) {
+        order.addProduct(product.idProduct, product.quantity);
+    }
+    return res.json({ total: order.totalPrice });
+});
---
+++ test/main.test.js
@@ -0,0 +1,25 @@
+import axios from "axios";
+import expect from "expect";
+
+test('Should not create a order with invalid CPF', async () => {
+    const input = {
+        cpf: '123.456.789-00'
+    }
+    const response = await axios.post('http://localhost:3000/checkout', input);
+    const output = response.data;
+    expect(output.message).toBe('Invalid CPF');
+});
+
+test("Should create an order with 3 products", async () => {
+    const input = {
+        cpf: '123.456.789-09',
+        products: [
+            { idProduct: 1, quantity: 1 },
+            { idProduct: 2, quantity: 1 },
+            { idProduct: 3, quantity: 3 }
+        ]
+    }
+    const response = await axios.post('http://localhost:3000/checkout', input);
+    const output = response.data;
+    expect(output.total).toBe(12);
+});
